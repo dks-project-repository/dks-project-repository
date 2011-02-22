13c05681eff9c9d33aa91461c9bf8c1f7c59fcbd
sbarnett87 <sbarnett87@28314c55-5b4b-0410-b444-2fc7fe15ad56>
2011-02-22 04:05:10 +0000

    Decoupled movement rate from frame rate. Added Drawable/Movable/Inputable interfaces. Added Camera class.

--- trunk/DigBuild/DigBuild/Cube.cpp
+++ trunk/DigBuild/DigBuild/Cube.cpp
@@ -6,29 +6,50 @@ Cube::Cube(Vector3 pos, unsigned int color)
 
 	m_rPos = pos;
 	m_rRot = Vector3();
+
+  drawMode = GL_QUADS;
+  RotateDirection = 1;
+  rotation = 0;
 }
 
 Cube::~Cube()
 {
 }
 
-void Cube::Tick()
+void Cube::HandleInput(const SDL_Event& event)
 {
+  if (event.type == SDL_KEYDOWN && event.key.keysym.sym == SDLK_r)
+  {
+		if (drawMode == GL_QUADS)
+		{
+			drawMode = GL_LINE_LOOP;
+		}
+		else
+		{
+			drawMode = GL_QUADS;
+		}
+  }
 }
 
-void Cube::Rotate()
+void Cube::Update(unsigned int ticks)
 {
-	glRotatef(m_rRot.X(), 1, 0, 0);
-	glRotatef(m_rRot.Y(), 0, 1, 0);
-	glRotatef(m_rRot.Z(), 0, 0, 1);
+  float rotateStep = RotateDirection * 0.1f * ticks;
+
+	rotation += rotateStep;
+  while (rotation >= 360)
+	  rotation -= 360;
+
+	SetRot(Vector3(0, rotation, 0));
 }
 
-void Cube::Draw(unsigned int drawMode)
+void Cube::Draw()
 {
 	glPushMatrix();
 
 	glTranslatef(m_rPos.X(), m_rPos.Y(), m_rPos.Z());
-	Rotate();
+	glRotatef(m_rRot.X(), 1, 0, 0);
+	glRotatef(m_rRot.Y(), 0, 1, 0);
+	glRotatef(m_rRot.Z(), 0, 0, 1);
 
 	float red = ((m_uColor & 0xff0000) >> 16) / 255;
 	float green = ((m_uColor & 0x00ff00) >> 8) / 255;
